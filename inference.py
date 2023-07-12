cc1b7f9179396d3298c8176db8f92207c9703be6
nguyen-brat <julius.2003.no.2@gmail.com>
2023-07-12 23:08:15 +0700

    first push

---
+++ inference.py
@@ -0,0 +1,68 @@
+import os
+from sentence_transformers.cross_encoder import CrossEncoder
+from trainer_rerank import trainer
+from another import DataloaderReranking
+from torch.utils.data import DataLoader
+from typing import List
+import numpy as np
+
+class xlm_roberta_reranking:
+    def __init__(
+            self,
+            batch_size=4,
+            sample_per_ques=4,
+            num_hard_negative_sample=1,
+            num_gold_sample=1,
+            MODEL:str='xlm-roberta-base',
+    ):
+        if os.path.exists('reranking_model'):
+            self.model = CrossEncoder('reranking_model')
+        else:
+            train_object = DataloaderReranking(
+                tokenize=MODEL, # tokenize version
+                config=MODEL, # tokenizer version
+                pth_raw_ctx = 'raw_context.json',
+                pth_sample_ctx = 'raw_sample_data.json',
+                batch_size=batch_size,
+                sample_per_ques=sample_per_ques,
+                num_hard_negative_sample=num_hard_negative_sample,
+                num_gold_sample=num_gold_sample,
+            )
+            train_sample = train_object()
+            train_dataloader = DataLoader(train_sample, batch_size=4, shuffle=False)
+            train = trainer(
+                train_dataloader,
+                MODEL,
+            )
+            train(epochs=10)
+            self.model = train.model
+    
+    def __call__(self, question:str, answers:List):
+        reranking_score = []
+        for answer in answers:
+            pair = [question, answer]
+            result = self.softmax(self.model.predict(pair))[1]
+            reranking_score.append(result)
+        sort_index = np.argsort(np.array(reranking_score))
+        reranking_answer = list(np.array(answers)[sort_index])
+        reranking_answer.reverse()
+        return reranking_answer
+    
+    @staticmethod
+    def softmax(x):
+        """Compute softmax values for each sets of scores in x."""
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
+
+if __name__ == "__main__":
+    reranking = xlm_roberta_reranking()
+    print(reranking(
+        "có cần chứng chỉ tiếng anh để  tốt nghiệp không ?",
+        [
+            "bắt buộc phải có để hoàn thành việc học",
+            "sinh viên phải có trình độ tiếng anh ít nhất 6.0 ilets hoặc tương đương khi tốt nghiệp",
+            "chứng chỉ tiếng anh là bắt buộc để tốt nghiệp",
+            "mặt trời mọc ở đằng tây thiên hạ ngạc nhiên chuyện là này",
+            "không bắt buộc phải có",
+        ]
+    ))
---
+++ trainer_rerank.py
@@ -0,0 +1,29 @@
+from torch.utils.data import DataLoader
+from sentence_transformers.cross_encoder import CrossEncoder
+from sentence_transformers.cross_encoder.evaluation import CESoftmaxAccuracyEvaluator
+from another import DataloaderReranking
+import math
+
+class trainer:
+    def __init__(
+            self,
+            dataloader,
+            MODEL:str='xlm-roberta-base',
+    ):
+        self.dataloader = dataloader
+        self.model = CrossEncoder(MODEL, num_labels=2)
+        self.warnmup_step = math.ceil(len(dataloader) * 10 * 0.1)
+    
+    def __call__(self, epochs:int=10):
+        self.model.fit(
+            train_dataloader=self.dataloader,
+            epochs=epochs,
+            evaluation_steps=10000,
+            warmup_steps=self.warnmup_step,
+        )
+        self.model.save_pretrained("reranking_model") 
+
+if __name__ == "__main__":
+    pass
+
+    
